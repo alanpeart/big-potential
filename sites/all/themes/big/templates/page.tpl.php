fd328463af35832153914965296315e569873f70
alanpeart <apeart@gmail.com>
2014-05-18 07:32:06 +0100

    Reverting

--- sites/all/themes/big/templates/page.tpl.php
+++ sites/all/themes/big/templates/page.tpl.php
@@ -1,46 +1,26 @@
 <!--.page -->
 <div role="document" class="page">
 
+<header class="l-header" id="user-menu-bar">
+	<section class="row">
+	   <?php if ($top_bar_secondary_menu): ?>
+        <nav id="secondary-menu" class="navigation" role="navigation">
+          <?php print $top_bar_secondary_menu; ?>
+        </nav> <!-- /#secondary-menu -->
+      <?php endif; ?>
+	</section>
+</header>
   <!--.l-header region -->
   <header role="banner" class="l-header">
-
-    <?php if ($top_bar): ?>
-      <!--.top-bar -->
-      <?php if ($top_bar_classes): ?>
-      <div class="<?php print $top_bar_classes; ?>">
-      <?php endif; ?>
-        <nav class="top-bar" data-topbar <?php print $top_bar_options; ?>>
-          <ul class="title-area">
-            <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
-            <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
-          </ul>
-          <section class="top-bar-section">
-            <?php if ($top_bar_main_menu) :?>
-              <?php print $top_bar_main_menu; ?>
-            <?php endif; ?>
-            <?php if ($top_bar_secondary_menu) :?>
-              <?php print $top_bar_secondary_menu; ?>
-            <?php endif; ?>
-          </section>
-        </nav>
-      <?php if ($top_bar_classes): ?>
-      </div>
-      <?php endif; ?>
-      <!--/.top-bar -->
-    <?php endif; ?>
-
     <!-- Title, slogan and menu -->
-    <?php if ($alt_header): ?>
-    <section class="row <?php print $alt_header_classes; ?>">
+    <section class="row <?php if (!$top_bar) { print $alt_header_classes; } ?>">
 
       <?php if ($linked_logo): print $linked_logo; endif; ?>
 
       <?php if ($site_name): ?>
         <?php if ($title): ?>
-          <div id="site-name" class="element-invisible">
-            <strong>
+          <div id="site-name">
               <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong>
           </div>
         <?php else: /* Use h1 when the content title is empty */ ?>
           <h1 id="site-name">
@@ -48,12 +28,35 @@
           </h1>
         <?php endif; ?>
       <?php endif; ?>
-
-      <?php if ($site_slogan): ?>
-        <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
+	  
+    <?php if (!empty($page['header'])): ?>
+      <!--.l-header-region -->
+        <div class="large-6 columns siteheader">
+          <?php print render($page['header']); ?>
+        </div>
+      <!--/.l-header-region -->
+    <?php endif; ?>
+	<!--<div id="mobile-bar"><div id="mobile-logo"><a href="/"><img src="/sites/all/themes/enas/images/enas_e.png" alt="ENAS" /></a></div><div id="mobile-menu-link"><a href="#mobile-menu"><img src="/sites/all/themes/enas/images/mobilemenu.png" alt="Menu" /></a></div></div>-->
+    </section>
+	<section id="menubar">
+    <?php if ($top_bar): ?>
+      <!--.top-bar -->
+      <?php if ($top_bar_classes): ?>
+      <div class="<?php print $top_bar_classes; ?>">
       <?php endif; ?>
-
-      <?php if ($alt_main_menu): ?>
+        <nav id="main-menu" class="top-bar navigation"<?php print $top_bar_options; ?>>
+          <section class="top-bar-section">
+            <?php if ($top_bar_main_menu) :?>
+              <?php print $top_bar_main_menu; ?>
+            <?php endif; ?>
+          </section>
+        </nav>
+      <?php if ($top_bar_classes): ?>
+      </div>
+      <?php endif; ?>
+      <!--/.top-bar -->
+	<?php else: ?>
+	    <?php if ($alt_main_menu): ?>
         <nav id="main-menu" class="navigation" role="navigation">
           <?php print ($alt_main_menu); ?>
         </nav> <!-- /#main-menu -->
@@ -64,20 +67,10 @@
           <?php print $alt_secondary_menu; ?>
         </nav> <!-- /#secondary-menu -->
       <?php endif; ?>
-
-    </section>
     <?php endif; ?>
+	
+	 </section>
     <!-- End title, slogan and menu -->
-
-    <?php if (!empty($page['header'])): ?>
-      <!--.l-header-region -->
-      <section class="l-header-region row">
-        <div class="large-12 columns">
-          <?php print render($page['header']); ?>
-        </div>
-      </section>
-      <!--/.l-header-region -->
-    <?php endif; ?>
 
   </header>
   <!--/.l-header -->
@@ -121,8 +114,8 @@
       <?php endif; ?>
 
       <a id="main-content"></a>
-
-      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+	  
+	  <!-- breadcrumbs were here -->
 
       <?php if ($title && !$is_front): ?>
         <?php print render($title_prefix); ?>
@@ -202,21 +195,15 @@
     <!--/.footer-columns-->
   <?php endif; ?>
 
+<?php if (!empty($page['footer'])): ?>  
   <!--.l-footer-->
   <footer class="l-footer panel row" role="contentinfo">
-    <?php if (!empty($page['footer'])): ?>
       <div class="footer large-12 columns">
         <?php print render($page['footer']); ?>
       </div>
-    <?php endif; ?>
-
-    <?php if ($site_name) :?>
-      <div class="copyright large-12 columns">
-        &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
-      </div>
-    <?php endif; ?>
   </footer>
   <!--/.footer-->
+ <?php endif; ?>  
 
   <?php if ($messages && $zurb_foundation_messages_modal): print $messages; endif; ?>
 </div>
