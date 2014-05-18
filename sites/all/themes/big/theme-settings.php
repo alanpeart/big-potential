6a13395ec127eff7cf00214234da8da8891b4bd3
alanpeart <apeart@gmail.com>
2014-05-18 07:15:12 +0100

    Updated zurb to try to fix top bar problem

--- sites/all/themes/big/theme-settings.php
+++ sites/all/themes/big/theme-settings.php
@@ -7,4 +7,4 @@
  * @param $form_state
  *   The form state.
  */
-function big_form_system_theme_settings_alter(&$form, &$form_state) {}
+function STARTER_form_system_theme_settings_alter(&$form, &$form_state) {}
